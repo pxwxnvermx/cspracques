6446e6bb21300990a42e07f2d9a04076919562cd
Pawan Verma <31195143+PawanVerma1337@users.noreply.github.com>
2018-09-14 14:57:52 +0530

    Create question6.cpp

---
+++ cplusplus/src/question6.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void checkprime(int x){
+	int count,i;
+	if(x==0){
+		cout << "no is prime";
+	}else{
+		for(i=2;i<=x/2;i++){
+			if(x%i==0)
+				count++;
+		}
+	}
+	if(count<1)
+		cout<<"No not is prime";
+	else{
+		cout <<"no is prime";
+	}	
+}
+
+int main(){
+	int num;
+	cout<<"Enter a no.";
+	cin>>num;
+	checkprime(num);
+	return 0;
+}
